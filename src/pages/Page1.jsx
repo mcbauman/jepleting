9d6bd56a677c18f410d5e4b99fb1a55aa1d01dd6
mcbauman <m.c.baumann@icloud.com>
2022-04-26 18:33:54 +0200

    table of progress

--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -2,17 +2,106 @@ import trans from "../components/lang"
 import {Context} from "../context/langContext.js"
 import { useContext } from "react"
 
+
 export default function Page1(){
     const {lang}=useContext(Context)
     return(
-        <div>
-            <h1>{trans[lang].page1}</h1>
-            <h1>API integration like maps or wather</h1>
-            <h1>or about me</h1>
-            <ul>
-                <li>projects</li>
-                <li>useReducer</li>
-            </ul>
-        </div>
+        <>
+            <header>
+                <h1>Matthias Baumann</h1>
+                <h2>Web-Developer</h2>
+            </header>
+            <aside>
+                <h3>{trans[lang].underConst}</h3>
+            </aside>
+            <main>
+                <table>
+                    <tr>
+                        <th>steps</th>
+                        <th>status</th>
+                        <th>date</th>
+                        <th>used</th>
+                    </tr>
+                    <tr>
+                        <td>✅</td>
+                        <td>Lebenslauf erstellt</td>
+                        <td>24.04.2022</td>
+                        <td>FontAwesome, html, css</td>
+                    </tr>
+                    <tr>
+                        <td>✅</td>
+                        <td>elementare Backendfuntionen</td>
+                        <td>24.04.2022</td>
+                        <td>Express(),Mongoose.Schema(), .env</td>
+                    </tr>
+                    <tr>
+                        <td>✅</td>
+                        <td>Mongoo Datenbank</td>
+                        <td>24.04.2022</td>
+                        <td>MongoDB Compass, cloud-host</td>
+                    </tr>
+                    <tr>
+                        <td>✅</td>
+                        <td>Grundlegendes Frontend</td>
+                        <td>25.04.2022</td>
+                        <td>React, Node-js, </td>
+                    </tr>
+                    <tr>
+                        <td>✅</td>
+                        <td>Übersetzung English/Deutsch</td>
+                        <td>25.04.2022</td>
+                        <td>useContext</td>
+                    </tr>
+                    <tr>
+                        <td>✅</td>
+                        <td>Basis-Styling</td>
+                        <td>26.04.2022</td>
+                        <td>css, scss</td>
+                    </tr>
+                    <tr>
+                        <td>✅</td>
+                        <td>Website geht online</td>
+                        <td>26.04.2022</td>
+                        <td>Ionos, Netlify</td>
+                    </tr>
+                    <tr>
+                        <td>⚠️</td>
+                        <td>Domain zuweisen</td>
+                        <td>26.04.2022</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>⚠️</td>
+                        <td>Routing verbessern</td>
+                        <td>26.04.2022</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>⚠️</td>
+                        <td>BackEnd online bringen</td>
+                        <td>27.04.2022</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>⚠️</td>
+                        <td>Responsivness</td>
+                        <td>27.04.2022</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>❓</td>
+                        <td>Wetter oder Maps</td>
+                        <td>01.05.2022</td>
+                        <td>API</td>
+                    </tr>
+                    <tr>
+                        <td>❓</td>
+                        <td>Landing Page</td>
+                        <td>01.05.2022</td>
+                        <td>CSS-Animations</td>
+                    </tr>
+                </table>
+            </main>
+        </>
     )
 }
